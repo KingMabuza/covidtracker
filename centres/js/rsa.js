54854023b7dd844696a99f6446509eb5ee4228bd
amallevi <65823576+amallevi@users.noreply.github.com>
2020-05-26 08:18:02 -0700

    Update rsa.js

--- centres/js/rsa.js
+++ centres/js/rsa.js
@@ -2,67 +2,89 @@ window.onload = function() {
 	RSA();
 }
 	
-	function RSA(){
-	fetch('https://covid-za-api.herokuapp.com/cases/timeline/provincial/cumulative')
-		.then(function (resp) {
-			return resp.json()
-		})
-		.then(function (data) {
-			
-		console.log(data);
-		let tot = data[data.length -1].total;
-		let tot2 = data[data.length -2].total;
-		document.getElementById('confirmed_cases').innerHTML = data[data.length - 1].total;
-			
-		let chart = document.getElementById('provincialChart').getContext('2d');
-		new Chart(chart,{
-			type:'bar',
-			data:{
-				labels:['Gauteng', 'Western_Cape', 'KwaZulu Natal', 'Eastern Cape', 'Free state', 'Mpumalanga', 'North West', 'Northern Cape', 'Limpopo'],
-				datasets:[{
-					label:'Cases',
-					data:[
-						data[data.length -1].provinces.gauteng,
-						data[data.length -1].provinces.western_cape,
-						data[data.length -1].provinces.kwazulu_natal,
-						data[data.length -1].provinces.eastern_cape,
-						data[data.length -1].provinces.free_state,
-						data[data.length -1].provinces.mpumlanga,
-						data[data.length -1].provinces.north_west,
-						data[data.length -1].provinces.northern_cape,
-						data[data.length -1].provinces.limpopo
-					],
-					backgroundColor:'red'
-				}]
-			},
+	function RSA() {
+		fetch('https://covid-za-api.herokuapp.com/cases/timeline/provincial/cumulative')
+			.then(function (resp) {
+				return resp.json()
+			})
+			.then(function (data) {
 
-			options:{
-				responsive:true,
-				maintainAspectRatio:false,
-				title:{
-					display:true,
-					text:'Provincial Cases',
-					fontSize:25
-				},
-				legend:{
-					display:false,
-				},
-			}
-		})
-			
-		})
+				console.log(data);
+				//total cases, today cases, provincial cases
+				let tot3 = data[data.length -1].total - data[data.length -2].total;
+				document.getElementById('confirmed_cases').innerHTML = data[data.length - 1].total;
+				document.getElementById('today_cases').innerHTML =  tot3;
 
-		.catch(function () {
-			
-            console.log('error');
-		})
-		fetch("https://pomber.github.io/covid19/timeseries.json")
-			.then(response => response.json())
-			.then(data => {
-				data["South Africa"].forEach(({date, confirmed, recovered, deaths}) =>
-					document.getElementById('recovered').innerHTML = recovered,
-					
-				);
-			});
+				let chart = document.getElementById('provincialChart').getContext('2d');
+				new Chart(chart, {
+					type: 'bar',
+					data: {
+						labels: ['Gauteng', 'Western_Cape', 'KwaZulu Natal', 'Eastern Cape', 'Free state', 'Mpumalanga', 'North West', 'Northern Cape', 'Limpopo'],
+						datasets: [{
+							label: 'Cases',
+							data: [
+								data[data.length - 1].provinces.gauteng,
+								data[data.length - 1].provinces.western_cape,
+								data[data.length - 1].provinces.kwazulu_natal,
+								data[data.length - 1].provinces.eastern_cape,
+								data[data.length - 1].provinces.free_state,
+								data[data.length - 1].provinces.mpumlanga,
+								data[data.length - 1].provinces.north_west,
+								data[data.length - 1].provinces.northern_cape,
+								data[data.length - 1].provinces.limpopo
+							],
+							backgroundColor: 'red'
+						}]
+					},
 
-	}
+					options: {
+						responsive: true,
+						maintainAspectRatio: false,
+						title: {
+							display: true,
+							text: 'Provincial Cases',
+							fontSize: 25
+						},
+						legend: {
+							display: false,
+						},
+					}
+				})
+
+			})
+
+			.catch(function () {
+
+				console.log('error');
+			})
+		
+
+		//total tests,icu,deaths,today deaths,timestamp
+
+		fetch("https://covid-za-api.herokuapp.com/cases/timeline/tests")
+			.then(function (resp) {
+				return resp.json()
+			})
+			.then(function (data) {
+				let death3 = data[data.length -1].deaths - data[data.length -2].deaths;
+				document.getElementById('tests').innerHTML = data[data.length - 1].cumulative_tests,
+				document.getElementById('critical').innerHTML = data[data.length - 1].critical_icu,
+				document.getElementById('deaths').innerHTML = data[data.length - 1].deaths,
+				document.getElementById('today_deaths').innerHTML = death3,
+				document.getElementById('timestamp').innerHTML = 'last update: ' + data[data.length - 1].date;
+				if(death3 <0){
+					document.getElementById('today_deaths').innerHTML = 0;
+				}
+				console.log(data);
+	})
+			//recovered,active cases
+		fetch("https://covid19.mathdro.id/api/countries/south africa/recovered")
+			.then(function (resp) {
+				return resp.json()
+			})
+			.then(function (data) {
+				document.getElementById('recovered').innerHTML = data[0].recovered,
+				document.getElementById('active_cases').innerHTML = data[0].active,
+				console.log(data);
+			})
+}
